7e18e478e588f0ed231bf67bda2c7610cfe2668b
dreamdr-developer <116975195+dreamdr-developer@users.noreply.github.com>
2023-02-14 04:49:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+gsap.to(".cntr",{
+    // opacity:0,
+    duration:1.5,
+    delay:0.4,
+    // y:60
+    height: "100%",
+    // y: 100,
+})
+gsap.to(".imgl1",{
+
+})
+// document.getElementById("#imgl1").style.transform="translateX(50deg)"
